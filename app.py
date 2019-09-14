e0dc05ce5fdd5838c29a1d19813c7c0d1713c750
mregger <eduardoegger@gmail.com>
2019-09-14 16:43:52 -0300

    feat(app.py) now uses websockets

--- app.py
+++ app.py
@@ -2,27 +2,24 @@
 #
 #by Eduardo"
 
-import sys
-from flask import Flask, request
-app = Flask(__name__)
-
+import asyncio
+import websockets
 from chatterbot import ChatBot
 trillian = ChatBot('Trillian',
 	storage_adapter='chatterbot.storage.SQLStorageAdapter',
 	database_url='./trillian.db')
 
-@app.route('/', methods=['GET'])
-def get_response():
-    pass
-    message = request.args.get('message')
-    #return message.upper() + '\n'
-    return str(trillian.get_response(message)) + '\n'
+async def handle_message(m):
+	return str(trillian.get_response(m))+'\n'
+
+async def on_message(ws, path):
+	while True:
+		m = await ws.recv()
+		r = await handle_message(m)
+		await ws.send(m)
 
 if __name__ == '__main__':
-    pass
-    if len(sys.argv) > 1 and sys.argv[1] == 'lts':
-        app.run(debug=False, port=80, host='0.0.0.0')
-    elif len(sys.argv) > 1 and sys.argv[1] == 'help':
-        print('To run in debug mode, use the command\n\t$ ./app.py\nTo run in production mode:\n\t $ sudo ./app.py lts&')
-    else:
-        app.run(debug=True, port=5000)
+	pass
+	server = websockets.serve(on_message, "localhost", 5000)
+	asyncio.get_event_loop().run_until_complete(server)
+	asyncio.get_event_loop().run_forever()
